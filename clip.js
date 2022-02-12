89680fa129126b851db46a7b1c58a4e5d64515de
Anthony <contact@anthonymayfield.com>
2022-02-12 17:47:28 +0100

    split properly

--- clip.js
+++ clip.js
@@ -7,8 +7,8 @@ const image = sharp(originalFilename);
 const rows = 13;
 const columns = 9;
 
-const imageWidth = 70;
-const imageHeight = 140;
+const imageWidth = 140;
+const imageHeight = 70;
 
 const totalKb = 2100;
 const totalTargetSize = 500;
@@ -28,10 +28,21 @@ createdArray.forEach(function (value, i) {
 
   let amountOfRowsToHit = amountOfRowsPerSplit;
 
+  const topPosition = (value - 1) * amountOfRowsToHit
+
   if(value == createdArray.length){
     amountOfRowsToHit = amountOfRowsToHit + remainder
     console.log('add remainder');
   }
+
+  console.log(topPosition);
+  console.log(amountOfRowsToHit);
+
+  image
+    .extract({ left: 0, top: topPosition * imageHeight, width: totalWidth, height: amountOfRowsToHit * imageHeight })
+    .toFile(`./output/${value}.webp`, function(err) {
+      // Save the top of the image to a file named "top.jpg"
+    });
 
   console.log(`${value} | ${amountOfRowsToHit}`);
 });
